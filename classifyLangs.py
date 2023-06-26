25db987ce0b162df9b865689d038c09141711e7f
pgeorgis <philip.georgis@gmail.com>
2023-06-26 19:37:06 +0200

    Added some summary logging

--- classifyLangs.py
+++ classifyLangs.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     parser.add_argument('--combine_diphthongs', dest='combine_diphthongs', action='store_true', help='Performs IPA string segmentation including diphthongs as single segmental units')
     parser.add_argument('--newick', dest='newick', action='store_true', help='Returns a Newick tree instead of a dendrogram')
     parser.add_argument('--exclude', default=None, nargs='+', help='Languages from CLDF data file to exclude')
-    parser.add_argument('--min_amc', default=0.6, help='Minimum average mutual coverage among doculects: doculect with lowest coverage is dropped until minimum value is reached')
+    parser.add_argument('--min_amc', default=0.7, help='Minimum average mutual coverage among doculects: doculect with lowest coverage is dropped until minimum value is reached')
     parser.add_argument('--outtree', default=None, help='Output file to which Newick tree string should be written')
     parser.set_defaults(
         ignore_stress=False,
@@ -77,6 +77,14 @@ if __name__ == "__main__":
                          ignore_stress=args.ignore_stress,
                          combine_diphthongs=args.combine_diphthongs
                          )
+
+    # Print some summary info about the loaded dataset
+    logger.info(f'Loaded {len(family.languages)} doculects.')
+    amc = family.calculate_mutual_coverage()[-1]
+    if amc <= 0.7:
+        logger.warning(f'Average mutual coverage = {round(amc, 2)}. Recommend minimum is 0.7.')
+    else:
+        logger.debug(f'Average mutual coverage = {round(amc, 2)}.')
 
     # Load or calculate phoneme PMI
     logger.info(f'Loading {family.name} phoneme PMI...')
