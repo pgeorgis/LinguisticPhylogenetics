802394faac9b14905c026393dfcb52d1a29ca2b5
Vitalijus Dobrovolskis <vitalijus@dobrovolskis.com>
2024-10-06 17:15:12 +0200

    Move submodule init logic from setup.sh to CI config

--- setup.sh
+++ setup.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+# Initialize and update submodules
+git submodule update --init --recursive
+
 # Create the virtual environment:
 python3 -m venv venv
 
-if declare -p GITHUB_USER_NAME >&/dev/null && declare -p GITHUB_ACCESS_TOKEN >&/dev/null; then
-    git config --global credential.helper store
-    git credential approve <<EOF
-protocol=https
-host=github.com
-username=${GITHUB_USER_NAME}
-password=${GITHUB_ACCESS_TOKEN}
-EOF
-fi
-
-git submodule update --init --recursive
-
-# Activate virtual environment, install packages, and update submodules:
+# Activate virtual environment, install packages
 source venv/bin/activate
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
