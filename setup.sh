eac7c321c8bd4478d2d59df59cbf84c486434a07
Vitalijus Dobrovolskis <vitalijus@dobrovolskis.com>
2024-10-06 15:54:06 +0200

    Add CI pipeline config and update numpy to 1.26.4 due to Python 3.12

--- setup.sh
+++ setup.sh
@@ -1,8 +1,24 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
 # Create the virtual environment:
 python3 -m venv venv
 
+if [ -z "$GITHUB_ACCESS_TOKEN" ] && [ -z "$GITHUB_USER_NAME" ]; then
+    touch "$HOME"/.git-credentials
+    chmod 0600 "$HOME"/.git-credentials
+cat <<EOF >> "$HOME/.git-credentials"
+[credential]
+    username = $GITHUB_USER_NAME
+    password = $GITHUB_ACCESS_TOKEN
+EOF
+    git config --global credential.helper store
+fi
+
 # Activate virtual environment, install packages, and update submodules:
 source venv/bin/activate && \
-    pip3 install -r requirements.txt && \
+    pip install --upgrade pip && \
+    pip install --upgrade setuptools && \
+    pip install -r requirements.txt && \
     git submodule init && \
     git submodule update
