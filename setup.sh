c7d44565cdfcbbf9b1b627de111a276c48dac902
Vitalijus Dobrovolskis <vitalijus@dobrovolskis.com>
2024-10-06 16:50:24 +0200

    Experiment 2 to fix submodule login from CI

--- setup.sh
+++ setup.sh
@@ -5,22 +5,18 @@ set -eou pipefail
 python3 -m venv venv
 
 if declare -p GITHUB_USER_NAME >&/dev/null && declare -p GITHUB_ACCESS_TOKEN >&/dev/null; then
-    CREDENTIALS_FILE="$HOME/.git-credentials"
-    git config --global --replace-all credential.helper store
-    touch "$CREDENTIALS_FILE"
-    chmod 0600 "$CREDENTIALS_FILE"
-cat <<EOF >> "$CREDENTIALS_FILE"
-[credential "https://github.com"]
-    username = $GITHUB_USER_NAME
-    password = $GITHUB_ACCESS_TOKEN
+    git config --global credential.helper store
+    git credential approve <<EOF
+protocol=https
+host=github.com
+username=${GITHUB_USER_NAME}
+password=${GITHUB_ACCESS_TOKEN}
 EOF
 fi
 
-git submodule init && \
-    git submodule update
+git submodule update --init --recursive
 
 # Activate virtual environment, install packages, and update submodules:
-source venv/bin/activate && \
-    pip install --upgrade pip && \
-    pip install --upgrade setuptools && \
-    pip install -r requirements.txt
+source venv/bin/activate
+pip install --upgrade pip setuptools
+pip install -r requirements.txt
