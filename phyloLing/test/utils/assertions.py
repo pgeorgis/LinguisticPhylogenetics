234348f3c07584546891f82db40f4f08238d1c4b
Vitalijus Dobrovolskis <vitalijus@dobrovolskis.com>
2025-02-11 15:55:50 +0100

    Fix test value check by decimal places

--- phyloLing/test/utils/assertions.py
+++ phyloLing/test/utils/assertions.py
@@ -16,20 +16,26 @@ from phyloLing.test.utils.types import (DistanceMatrix, ExecutionResult,
                                         TreeDistance, TreeDistanceMapper)
 
 logging.basicConfig(level=default_logging_level, format=default_logging_format)
-logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
+
+_max_value_difference = 10 ** -decimal_places
+_float_format_string = f".{decimal_places + 1}f"
 
 
-def format_float(value: float,
-                 places: int = decimal_places + 1) -> str:
-    return f"{value:.{places}f}"
+def _format_float(value: float) -> str:
+    return f"{value:{_float_format_string}}"
 
 
-def format_elapsed_time(time: timedelta) -> str:
+def _format_elapsed_time(time: timedelta) -> str:
     return str(time).split('.')[0]
 
 
-def format_datetime(time: datetime) -> str:
+def _format_datetime(time: datetime) -> str:
     return time.astimezone(test_output_time_zone).strftime("%Y-%m-%d %H:%M:%S")
+
+
+def _is_close(a: float, b: float) -> bool:
+    return math.isclose(a, b, abs_tol=_max_value_difference)
 
 
 def _assert_distance_matrices_equal(test: unittest.TestCase,
@@ -37,20 +43,18 @@ def _assert_distance_matrices_equal(test: unittest.TestCase,
                                     second: DistanceMatrix,
                                     fail_if_distance_matrices_differ: bool) -> None:
     test.assertCountEqual(first.keys(), second.keys(), "The number of language pairs is not equal.")
-
-    max_delta=10 ** -decimal_places
-
     different = []
     identical = []
     for (lang1, lang2), expected_value in first.items():
         if (lang1, lang2) not in second and (lang2, lang1) not in second:
             test.fail(f"Pair ({lang1}, {lang2}) is missing.")
         actual_value = second.get((lang1, lang2), second.get((lang2, lang1)))
+        abs_diff = abs(expected_value - actual_value)
         output_row = [f"{lang1} - {lang2}",
                       expected_value,
                       actual_value,
-                      abs(expected_value - actual_value)]
-        if math.isclose(expected_value, actual_value, rel_tol=max_delta):
+                      abs_diff]
+        if _is_close(expected_value, actual_value):
             identical.append(output_row)
         else:
             different.append(output_row)
@@ -58,7 +62,7 @@ def _assert_distance_matrices_equal(test: unittest.TestCase,
     output_columns = ['Language pair', 'Expected', 'Actual', 'Difference']
     summary: PrettyTable = PrettyTable(output_columns)
     summary.align = 'r'
-    summary.float_format = f".{decimal_places + 1}"
+    summary.float_format = _float_format_string
     different_count = len(different)
     if different:
         for i, row in enumerate(different):
@@ -68,13 +72,13 @@ def _assert_distance_matrices_equal(test: unittest.TestCase,
 
     if different:
         pairs_label = 'pair differs' if different_count == 1 else 'pairs differ'
-        message = f"Distance matrices are not equal, {different_count} {pairs_label} more than {max_delta}:\n{summary.get_string()}"
+        message = f"Distance matrices are not equal, {different_count} {pairs_label} more than {_max_value_difference}:\n{summary.get_string()}"
         if fail_if_distance_matrices_differ:
             test.fail(message)
         else:
-            logger.warning(message)
+            _logger.warning(message)
     else:
-        logger.info(f"Distance matrices are equal.")
+        _logger.info(f"Distance matrices are equal.")
 
 
 def _assert_tree_distances_equal(test: unittest.TestCase,
@@ -84,18 +88,17 @@ def _assert_tree_distances_equal(test: unittest.TestCase,
                                  distance_label: str) -> None:
     first_value = tree_distance_mapper(first)
     second_value = tree_distance_mapper(second)
-    max_difference = 10 ** -decimal_places
-    formatted_first_value = format_float(first_value)
-    formatted_second_value = format_float(second_value)
-    formatted_delta = format_float(abs(first_value - second_value))
+    formatted_first_value = _format_float(first_value)
+    formatted_second_value = _format_float(second_value)
+    delta = abs(first_value - second_value)
+    formatted_delta = _format_float(delta)
 
-    test.assertAlmostEqual(
-        first_value, second_value,
-        decimal_places,
-        f"{distance_label} tree distances are not equal: {formatted_first_value} != {formatted_second_value}, |Δ|: {formatted_delta}"
-    )
-    if math.isclose(first_value, second_value, rel_tol=max_difference):
-        logger.info(f"{distance_label} tree distances are equal.")
+    if _is_close(first_value, second_value):
+        _logger.info(f"{distance_label} tree distances are equal.")
+    else:
+        test.fail(f"{distance_label} tree distances are not equal: "
+                  f"{formatted_first_value} != {formatted_second_value}, |Δ|: {formatted_delta}")
+
 
 
 def _assert_all_tree_distances_equal(test: unittest.TestCase,
@@ -119,7 +122,7 @@ def _assert_results_equal(test: unittest.TestCase,
 
 def assert_determinism(test: unittest.TestCase,
                        initial_result: ExecutionResultInformation) -> None:
-    logger.info(f"Initial run done in {format_elapsed_time(initial_result.time_elapsed)}")
+    _logger.info(f"Initial run done in {_format_elapsed_time(initial_result.time_elapsed)}")
     last_result: ExecutionResult = initial_result.result
     language_family: str = initial_result.language_family
     test_config_file: str = initial_result.test_configuration_file
@@ -128,16 +131,16 @@ def assert_determinism(test: unittest.TestCase,
     last_iteration_time: timedelta | None = initial_result.time_elapsed
     for i in range(determinism_test_iterations):
         formatted_iteration: int = i + 1
-        logger.info(f"Running iteration {formatted_iteration} for {language_family}...")
+        _logger.info(f"Running iteration {formatted_iteration} for {language_family}...")
         if last_iteration_time is not None:
             remaining_iterations: int = determinism_test_iterations - i
             estimated_remaining_time_seconds: int = remaining_iterations * round(last_iteration_time.total_seconds())
             estimated_remaining_time = timedelta(
                 seconds=estimated_remaining_time_seconds,
             )
-            logger.info(f"Estimated time remaining: {str(estimated_remaining_time)}")
-            estimated_end_time = format_datetime(datetime.now() + estimated_remaining_time)
-            logger.info(f"Estimated finish time: {estimated_end_time}")
+            _logger.info(f"Estimated time remaining: {str(estimated_remaining_time)}")
+            estimated_end_time = _format_datetime(datetime.now() + estimated_remaining_time)
+            _logger.info(f"Estimated finish time: {estimated_end_time}")
 
         start_time: datetime = datetime.now()
         current_result: ExecutionResult = execute_classify_langs(
@@ -145,12 +148,12 @@ def assert_determinism(test: unittest.TestCase,
         )
         end_time: datetime = datetime.now()
         last_iteration_time = end_time - start_time
-        logger.info(f"Iteration {formatted_iteration} done in {format_elapsed_time(last_iteration_time)}")
+        _logger.info(f"Iteration {formatted_iteration} done in {_format_elapsed_time(last_iteration_time)}")
 
-        logger.info(f"Comparing with last iteration values...")
+        _logger.info(f"Comparing with last iteration values...")
         _assert_results_equal(test, current_result, last_result)
 
-        logger.info(f"Comparing with initial iteration values...")
+        _logger.info(f"Comparing with initial iteration values...")
         _assert_results_equal(test, current_result, initial_result.result)
 
         last_result = current_result
@@ -165,8 +168,8 @@ def assert_tree_distances_improved(test: unittest.TestCase,
         best_tree_distance: float = tree_distance_mapper(best_tree_distances[reference_tree])
         result_tree_distance: float = tree_distance_mapper(result.tree_distance)
 
-        best_tree_distance_formatted = format_float(best_tree_distance)
-        result_tree_distance_formatted = format_float(result_tree_distance)
+        best_tree_distance_formatted = _format_float(best_tree_distance)
+        result_tree_distance_formatted = _format_float(result_tree_distance)
 
         reference_tree_name = reference_tree.split('/')[-1]
         if result_tree_distance > best_tree_distance:
@@ -175,7 +178,7 @@ def assert_tree_distances_improved(test: unittest.TestCase,
                 f"{result_tree_distance_formatted} > {best_tree_distance_formatted}"
             )
         else:
-            logger.info(
+            _logger.info(
                 f"{tree_distance_label} distance is less or equal to the best tree distance for {reference_tree_name}: "
                 f"{result_tree_distance_formatted} <= {best_tree_distance_formatted}"
             )
